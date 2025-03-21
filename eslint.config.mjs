f1d070ed8f58d8eaecba0e2265a114a0cd71c87d
CrackerGames <Tubbe.friese@gmail.com>
2025-03-21 14:41:02 +0100

    Update ESLint and Jest configurations for improved linting and testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import jest from 'eslint-plugin-jest';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -24,7 +25,6 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
@@ -33,6 +33,28 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-console': 'warn',
+      'no-extra-semi': 'error',
+      'no-unreachable': 'error',
+      'no-constant-condition': 'error',
+      'no-empty': 'error',
     },
   },
-]
+  {
+    files: ['tests/*', '**/*.test.js'],
+    plugins: { jest },
+    rules: {
+      ...jest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+];
