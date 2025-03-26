e4875379d96cff284347b28a9cef15044ab037da
elisabetasta <eolafs24@student.aau.dk>
2025-03-26 12:11:49 +0100

    Add NavigationView component and update routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import SearchPage from './SearchPage.jsx';
 import UserView from './UserView.jsx';
+import NavigationView from './NavigationView.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<SearchPage />} />
+      <Route path="/navigation" element={<NavigationView />} />
       <Route path="/directions" element={<UserView />} /> {/* Admin View */}
     </Routes>
     </BrowserRouter>
