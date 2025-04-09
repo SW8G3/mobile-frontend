3af728aa790019f6ab79369a09b5271a98e7c1e1
elisabetasta <eolafs24@student.aau.dk>
2025-04-09 11:22:17 +0200

    Add vite-plugin-mkcert for HTTPS support in development

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
+import mkcert from 'vite-plugin-mkcert';
 
 dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
+    https: true,
+    host: true,
     port: process.env.VITE_PORT || 5173,
   },
   define: {
