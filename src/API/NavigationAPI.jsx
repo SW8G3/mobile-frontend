68ca63722febd6f457e3af826ae3064e18ea8f00
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-04-22 11:37:19 +0200

    Update NavigationAPI.jsx

--- src/API/NavigationAPI.jsx
+++ src/API/NavigationAPI.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL, 
+    baseURL: '/graph'
 });
 
 const searchWithTag = async (searchTag) => {
@@ -9,7 +9,7 @@ const searchWithTag = async (searchTag) => {
         const data = {
             searchTag,
         };
-        const response = await api.post("/graph/search", data);
+        const response = await api.post("/search", data);
         return response.data;
     } catch (err) {
         console.error(err);
@@ -22,7 +22,7 @@ const getNodeFromId = async (nodeId) => {
         const data = {
             nodeId,
         };
-        const response = await api.post("/graph/node", data);
+        const response = await api.post("/node", data);
         return response.data;
     } catch (err) {
         console.error(err);
@@ -37,7 +37,7 @@ const getRoute = async (src, dst) => {
             src: parseInt(src),
             dst: parseInt(dst),
         };
-        const response = await api.post("/graph/route", data);
+        const response = await api.post("/route", data);
         return response.data;
     } catch (err) {
         console.error(err);
@@ -51,7 +51,7 @@ const getDirectionPhoto = async (src, dst) => {
             src: parseInt(src),
             dst: parseInt(dst),
         };
-        const response = await api.post("/graph/direction", data);
+        const response = await api.post("/direction", data);
         return response.data;
     } catch (err) {
         console.error(err);
