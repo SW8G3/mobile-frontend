1882fe0b6c53cdb18e8ff2f4851b4577a1ba7ee6
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2025-04-29 12:00:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
 import mkcert from 'vite-plugin-mkcert';
 import fs from 'fs';
 import path from 'path';
 
-dotenv.config();
+
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
